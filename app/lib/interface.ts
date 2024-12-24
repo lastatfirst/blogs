bd7af47ed08a72c54bd014a94cc521f3195cd071
vihanvt <vihan.vnt@gmail.com>
2024-12-24 15:15:28 +0530

    wtf

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -10,20 +10,23 @@ export interface simpleBlogCard {
     content: PortableTextBlock[];
   }
   
-  // Define the type for PortableText content structure
   export type PortableTextBlock = {
     _key: string;
     _type: string;
-    children: { _key: string; _type: string; text: string }[];
-    markDefs: MarkDef[];  // Replaced `any[]` with a specific type `MarkDef[]`
+    children: PortableTextChild[];
+    markDefs: MarkDef[];  // More specific type
     style: string;
   };
   
-  // Define the type for marks applied to text
+  export type PortableTextChild = {
+    _key: string;
+    _type: string;
+    text: string;
+  };
+  
   export type MarkDef = {
     _key: string;
     _type: string;
     href?: string;  // Optional property for links
-    [key: string]: any;  // Allow additional properties
   };
   

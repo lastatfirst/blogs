8cacc6f9af8fab85c7e603bb25fea2e957ac64fe
vihanvt <vihan.vnt@gmail.com>
2025-08-19 09:23:37 +0530

    foo size

--- app/components/ReadingProgress.tsx
+++ app/components/ReadingProgress.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function ReadingProgress() {
   const [progress, setProgress] = useState(0);
@@ -8,20 +8,22 @@ export default function ReadingProgress() {
   useEffect(() => {
     const updateReadProgress = () => {
       const winScroll = window.scrollY;
-      const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
       const scrolled = (winScroll / height) * 100;
       setProgress(scrolled);
     };
 
-    window.addEventListener('scroll', updateReadProgress);
+    window.addEventListener("scroll", updateReadProgress);
     updateReadProgress();
 
-    return () => window.removeEventListener('scroll', updateReadProgress);
+    return () => window.removeEventListener("scroll", updateReadProgress);
   }, []);
 
   return (
     <div className="fixed top-0 left-0 w-full h-[3px] bg-transparent z-50">
-      <div 
+      <div
         className="h-full bg-[#ea7477] transition-all duration-200"
         style={{ width: `${progress}%` }}
       />
